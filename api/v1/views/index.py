36e3f8faec707996fa5a4a3261f219aa9ff51a9d
Bappa-Kamba <bappahmuhammad18@gmail.com>
2024-01-09 17:19:58 +0100

    Add imports for models in index.py

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -4,6 +4,13 @@
 """
 from api.v1.views import app_views
 from flask import jsonify
+from models import storage
+from models.amenity import Amenity
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.user import User
 
 
 @app_views.route("/status", strict_slashes=False)
@@ -11,19 +18,12 @@ def status():
     """Return status"""
     return jsonify({
         "status": "OK"
-    })
+        })
 
 
 @app_views.route("/stats", strict_slashes=False)
 def stats():
     """Return stats"""
-    from models import storage
-    from models.amenity import Amenity
-    from models.city import City
-    from models.place import Place
-    from models.review import Review
-    from models.state import State
-    from models.user import User
     return jsonify({
         "amenities": storage.count(Amenity),
         "cities": storage.count(City),
