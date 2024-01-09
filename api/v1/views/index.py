b06f913c173065fa2ddbe80054b9325e442f8c17
Bappa-Kamba <bappahmuhammad18@gmail.com>
2024-01-09 17:15:53 +0100

    Refactor status and stats routes to return JSON responses

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -3,31 +3,32 @@
     Initializes the index module
 """
 from api.v1.views import app_views
-from models import storage
-from models.amenity import Amenity
-from models.city import City
-from models.place import Place
-from models.review import Review
-from models.state import State
-from models.user import User
+from flask import jsonify
 
 
 @app_views.route("/status", strict_slashes=False)
 def status():
     """Return status"""
-    return {
+    return jsonify({
         "status": "OK"
-        }
+    })
 
 
 @app_views.route("/stats", strict_slashes=False)
 def stats():
     """Return stats"""
-    return {
+    from models import storage
+    from models.amenity import Amenity
+    from models.city import City
+    from models.place import Place
+    from models.review import Review
+    from models.state import State
+    from models.user import User
+    return jsonify({
         "amenities": storage.count(Amenity),
         "cities": storage.count(City),
         "places": storage.count(Place),
         "reviews": storage.count(Review),
         "states": storage.count(State),
         "users": storage.count(User)
-    }
+    })
